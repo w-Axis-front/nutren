8aeedb54740fb852a0d27ea54fec9c1197634069
fedorova_kateryna <K_phedorova@ukr.net>
2021-02-02 18:48:13 +0200

    feat(main): add slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,9 +1,9 @@
 import slick from "slick-carousel";
 
 export default function slider() {
-    let slider1 = $('#js_banner-slider');
-        // current_slide = document.getElementById('js_reviews-current-slide'),
-        // current_slide_img = $('#js_current-slide-imgs');
+    const slider1 = $('#js_banner-slider');
+    // current_slide = document.getElementById('js_reviews-current-slide'),
+    // current_slide_img = $('#js_current-slide-imgs');
 
     $(slider1).slick({
         prevArrow: '<button type="button" class="banner-slider__prev"><img class="banner-slider__arrow-img" src="img/svg/arrow-left.svg" alt="Arrow left"/></button>',
@@ -18,6 +18,12 @@ export default function slider() {
         //     }
         // }]
     });
+
+    const bgColors = ["rgba(255, 216, 198, 0.42)", "rgba(197, 221, 235, 0.54)", "rgba(148, 204, 255, 0.35)", "rgba(235, 197, 220, 0.4)", "rgba(201, 93, 15, 0.3)"];
+    $(slider1).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        slider1.css({"background-color": bgColors[nextSlide]});
+    });
+
     // $(slider2).slick({
     //     slidesToShow: 5,
     //     slidesToScroll: 1,
@@ -42,33 +48,28 @@ export default function slider() {
     //     ]
     // });
 
-    // function functionMedia(mediaQuery) {
-    //     if (mediaQuery.matches) {
-    //         if (current_slide) {
-    //             $(slider2).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-    //
-    //                 $(current_slide_img).attr({
-    //                     "src": `/img/review${nextSlide+1}.png`
-    //                 });
-    //                 current_slide.classList.add("active");
-    //
-    //             });
-    //
-    //             $(slider2).on('afterChange', function (event, slick, currentSlide) {
-    //                 current_slide.classList.remove("active");
-    //             });
-
-                // current_slide.addEventListener("animationend", function () {
-                // 	current_slide.classList.remove("active");
-                // });
-
-    //         }
+//     function functionMedia(mediaQuery) {
+//         if (mediaQuery.matches) {
+//             if (current_slide) {
+//                 $(slider2).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+//
+//                     $(current_slide_img).attr({
+//                         "src": `/img/review${nextSlide+1}.png`
+//                     });
+//                     current_slide.classList.add("active");
+//
+//                 });
+//                 $(slider2).on('afterChange', function (event, slick, currentSlide) {
+//                     current_slide.classList.remove("active");
+//                 });
+//                 current_slide.addEventListener("animationend", function () {
+//                 	current_slide.classList.remove("active");
+//                 });
+//             }
     //     }
     // }
-    //
-    // let mediaQuery = window.matchMedia("(min-width: 1140px)");
-    // functionMedia(mediaQuery);
-    //
-    // mediaQuery.addListener(functionMedia);
-
+//     let mediaQuery = window.matchMedia("(min-width: 1140px)");
+//     functionMedia(mediaQuery);
+//     mediaQuery.addListener(functionMedia);
 }
+
