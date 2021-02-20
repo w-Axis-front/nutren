5d5c8f6fc4945bb8de494b7bdb82956d5b5bbaba
Kate Fedorova <k_phedorova@ukr.net>
2021-02-20 21:34:12 +0200

    feat(products): add animation on mobile scroll

--- src/js/modules/scrollContent.js
+++ src/js/modules/scrollContent.js
@@ -1,23 +1,42 @@
 export default function scrollContent() {
-    // function scrollContentHandler(media_check_max) {
-    //     if (media_check_max.matches) {
-    //         const productsMain = document.querySelector(".main-products");
-    //
-    //         window.addEventListener(
-    //             "scroll",
-    //             () => {
-    //                 productsMain.style.setProperty(
-    //                     "--scroll",
-    //                     window.pageYOffset / (productsMain.offsetHeight - window.innerHeight)
-    //                 );
-    //             },
-    //             false
-    //         );
-    //     }
-    // }
-    //
-    // const media_check_max = window.matchMedia("(max-width: 767.9px)");
-    // scrollContentHandler(media_check_max);
-    // media_check_max.addListener(scrollContentHandler);
+    function scrollContentHandler(media_check_max) {
+        if (media_check_max.matches) {
+            const productsMain = document.querySelector(".main-products");
+            const mainSlider = document.querySelector(".main-slider");
+            const animatedItems = document.querySelectorAll(".js_scroll-animate");
+
+            for(let idx = 0; idx < animatedItems.length; idx++) {
+                const animatedItem = animatedItems[idx];
+                if (idx === 0) {
+                    window.addEventListener(
+                        "scroll",
+                        () => {
+                            document.body.style.setProperty(
+                                "--scrollFirstCard",
+                                // window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+                                (window.pageYOffset - mainSlider.offsetHeight) / (animatedItem.offsetHeight)
+                            );
+                        },
+                        false
+                    );
+                } else if (idx === 1) {
+                    window.addEventListener(
+                        "scroll",
+                        () => {
+                            document.body.style.setProperty(
+                                "--scrollSecondCard",
+                                (window.pageYOffset - mainSlider.offsetHeight - animatedItem.offsetHeight) / (animatedItem.offsetHeight)
+                            );
+                        },
+                        false
+                    );
+                }
+            }
+        }
+    }
+
+    const media_check_max = window.matchMedia("(max-width: 767.9px)");
+    scrollContentHandler(media_check_max);
+    media_check_max.addListener(scrollContentHandler);
     return null;
 }
