1173b7873357fbee031206dbb8fce5defa69a237
fedorova_kateryna <K_phedorova@ukr.net>
2021-02-22 18:00:01 +0200

    feat(about-page): add

--- src/js/modules/masonryGrid.js
+++ src/js/modules/masonryGrid.js
@@ -4,13 +4,21 @@ import imagesLoaded from 'imagesloaded'
 export default function masonryGrid() {
     const grid = document.querySelector('.js_grid');
 
-    imagesLoaded( grid, function() {
-        // init Masonry after all images have loaded
-        const msnry = new Masonry(grid, {
-            itemSelector: '.grid-item',
-            columnWidth: '.grid-sizer',
-            gutter: 95,
-            percentPosition: true
-        })
-    });
+    function buildMasonry(media_check_min) {
+        if (media_check_min.matches) {
+            imagesLoaded( grid, function() {
+                // init Masonry after all images have loaded
+                const msnry = new Masonry(grid, {
+                    itemSelector: '.grid-item',
+                    columnWidth: '.grid-sizer',
+                    gutter: 95,
+                    percentPosition: true,
+                })
+            });
+        }
+    }
+
+    const media_check_min = window.matchMedia("(min-width: 768px)");
+    buildMasonry(media_check_min);
+    media_check_min.addListener(buildMasonry);
 }
--- src/js/modules/scrollContent.js
+++ src/js/modules/scrollContent.js
@@ -1,36 +1,30 @@
 export default function scrollContent() {
+    const styleProperties = ["--scrollFirstCard", "--scrollSecondCard", "--scrollThirdCard", "--scrollForthCard", "--scrollFifthCard", "--scrollSixthCard", "--scrollSeventhCard"];
+
+    function animateItemOnScroll(animatedItem, idx, mainSlider) {
+        window.addEventListener(
+            "scroll",
+            () => {
+                document.body.style.setProperty(
+                    styleProperties[idx],
+                    // window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+                    (window.pageYOffset - mainSlider.offsetHeight - animatedItem.offsetHeight * idx) / (animatedItem.offsetHeight)
+                    // (window.pageYOffset - mainSlider.offsetHeight - animatedItem.offsetHeight) / (animatedItem.offsetHeight)
+                );
+            },
+            false
+        );
+    }
+
     function scrollContentHandler(media_check_max) {
         if (media_check_max.matches) {
-            const productsMain = document.querySelector(".main-products");
+            // const productsMain = document.querySelector(".main-products");
             const mainSlider = document.querySelector(".main-slider");
             const animatedItems = document.querySelectorAll(".js_scroll-animate");
 
-            for(let idx = 0; idx < animatedItems.length; idx++) {
+            for (let idx = 0; idx < animatedItems.length; idx++) {
                 const animatedItem = animatedItems[idx];
-                if (idx === 0) {
-                    window.addEventListener(
-                        "scroll",
-                        () => {
-                            document.body.style.setProperty(
-                                "--scrollFirstCard",
-                                // window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-                                (window.pageYOffset - mainSlider.offsetHeight) / (animatedItem.offsetHeight)
-                            );
-                        },
-                        false
-                    );
-                } else if (idx === 1) {
-                    window.addEventListener(
-                        "scroll",
-                        () => {
-                            document.body.style.setProperty(
-                                "--scrollSecondCard",
-                                (window.pageYOffset - mainSlider.offsetHeight - animatedItem.offsetHeight) / (animatedItem.offsetHeight)
-                            );
-                        },
-                        false
-                    );
-                }
+                animateItemOnScroll(animatedItem, idx, mainSlider);
             }
         }
     }
@@ -38,5 +32,4 @@ export default function scrollContent() {
     const media_check_max = window.matchMedia("(max-width: 767.9px)");
     scrollContentHandler(media_check_max);
     media_check_max.addListener(scrollContentHandler);
-    return null;
 }
